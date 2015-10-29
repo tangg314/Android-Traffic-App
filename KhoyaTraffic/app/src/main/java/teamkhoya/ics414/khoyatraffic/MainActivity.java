d120f56136965dba348eacadec49b5153ca3e770
ashleyfa <ashleyfa@hawaii.edu>
2015-10-28 17:50:17 -1000

    testing

---
+++ KhoyaTraffic/app/src/main/java/teamkhoya/ics414/khoyatraffic/MainActivity.java
@@ -0,0 +1,13 @@
+package teamkhoya.ics414.khoyatraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
