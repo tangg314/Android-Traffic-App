b94aa336396109032a1b8273c14e2f1c88c06745
Melissa Gushiken <mgushike@hawaii.edu>
2015-11-08 21:12:02 -1000

    -Added a loop to cycle -Convert min to hour (to output mph) -Convert sec to ms (for cycle time)

--- KhoyaTraffic/app/src/main/java/teamkhoya/ics414/khoyatraffic/TrafficAsync.java
+++ KhoyaTraffic/app/src/main/java/teamkhoya/ics414/khoyatraffic/TrafficAsync.java
@@ -1,6 +1,10 @@
 package teamkhoya.ics414.khoyatraffic;
 
+import android.app.NotificationManager;
 import android.os.AsyncTask;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import java.net.URLConnection;
 import java.text.DecimalFormat;
 import java.net.URL;
@@ -19,31 +23,48 @@ public class TrafficAsync extends AsyncTask<String, Void, String> {
     protected String doInBackground(String... params) {
         String str_dist="";
         String str_time="";
-        try {
-            URL url = new URL(params[0]);
-            URLConnection uconnect = url.openConnection();
-            BufferedReader bReader = new BufferedReader(new InputStreamReader(uconnect.getInputStream()));
-            String line;
-            StringBuffer sb = new StringBuffer();
-            while((line = bReader.readLine()) != null){
-                sb.append(line);
+
+        Long currentTime = System.nanoTime();
+        Long estimatedTime = 0L;
+        Long waitMS = 60000L; //cycle time
+
+        do {
+            try {
+                URL url = new URL(params[0]);
+                URLConnection uconnect = url.openConnection();
+                BufferedReader bReader = new BufferedReader(new InputStreamReader(uconnect.getInputStream()));
+                String line;
+                StringBuffer sb = new StringBuffer();
+                while ((line = bReader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                JSONObject main = new JSONObject(sb.toString());
+                JSONArray rows_array = new JSONArray(main.getString("rows"));
+                JSONObject elements_object = rows_array.getJSONObject(0);
+                JSONArray elements_array = elements_object.getJSONArray("elements");
+                JSONObject distance_object = elements_array.getJSONObject(0);
+                JSONObject distance = distance_object.getJSONObject("distance");
+                str_dist = distance.getString("value"); //distance in meters
+                //str_dist = distance.getString("text"); // distance string in km
+
+                JSONObject time = distance_object.getJSONObject("duration");
+                str_time = time.getString("text"); //duration string in mins
+                //str_time = time.getString("value"); //duration in seconds
+
+                currentTime = System.currentTimeMillis();
+                estimatedTime = currentTime + convertMinutesToMilisec(Long.parseLong(str_time)); //compute eta in msec
+
+            } catch (java.io.IOException | JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                wait(waitMS); //cycle
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
 
-            JSONObject main = new JSONObject(sb.toString());
-            JSONArray rows_array = new JSONArray(main.getString("rows"));
-            JSONObject elements_object = rows_array.getJSONObject(0);
-            JSONArray elements_array = elements_object.getJSONArray("elements");
-            JSONObject distance_object = elements_array.getJSONObject(0);
-            JSONObject distance = distance_object.getJSONObject("distance");
-            str_dist = distance.getString("value"); //distance in meters
-            //str_dist = distance.getString("text"); // distance string in km
-
-            JSONObject time = distance_object.getJSONObject("duration");
-            str_time = time.getString("text"); //duration string in mins
-            //str_time = time.getString("value"); //duration in seconds
-        } catch (java.io.IOException | JSONException e) {
-            e.printStackTrace();
-        }
+        } while (currentTime < estimatedTime);
         return str_dist + ":" + str_time;
     }
 
@@ -52,8 +73,10 @@ public class TrafficAsync extends AsyncTask<String, Void, String> {
         String[] arr = result.split(":");
         DecimalFormat nfm = new DecimalFormat("#0.00");
         double miles = convertMetersToMiles(Double.parseDouble(arr[0]));
-        String printText = "Distance: " + String.valueOf(nfm.format(miles))+ " miles\nduration: " + arr[1];
+        double hours = convertMinutesToHours(Double.parseDouble(arr[1]));
+        String printText = "Distance: " + String.valueOf(nfm.format(miles))+ " miles\nduration: " + hours;
         //etaDis.setText(printText); need to make EditText or TextView whatever you prefer to print out the information
+
     }
 
     /**
@@ -68,5 +91,29 @@ public class TrafficAsync extends AsyncTask<String, Void, String> {
 
     /**
      * May need to make a method for converting seconds to hours
+     * takes the seconds from the json file and converts it to hours
+     * @param seconds
+     * @return hours
      */
+    public double convertSecondsToHours(double seconds) {
+        return seconds/3600;
+    }
+
+    /**
+     * takes the minutes from the json file and converts it to hours
+     * @param minutes
+     * @return hours
+     */
+    public double convertMinutesToHours(double minutes) {
+        return minutes/60;
+    }
+
+    /**
+     * converts minutes to msec
+     * @param minutes
+     * @return
+     */
+    public Long convertMinutesToMilisec(double minutes) {
+        return Double.doubleToLongBits(minutes) * 60000;
+    }
 }
